588cdc1fb18a1bcc4adc46ae0635d11e07769331
Sean Simpson <JediTiger@users.noreply.github.com>
2026-08-20 14:19:18 -0500

    Search page build in progress

--- assignment-app/src/search.jsx
+++ assignment-app/src/search.jsx
@@ -4,21 +4,24 @@ export default function Search() {
 
    const [searchObject, setSearchObject] = useState([]);
    useEffect(() => {
-   // Will need an 'api' call to fetch the search object
+   // Successfully calls the api to fetch the search object
       fetch('/api/data/search')
          .then((response) => response.text())
          .then((data) => {
-   // Will create the search var here
+   // Search data successfully stored in an accessible var
             setSearchObject(data);
         })
       .catch((error) => {
         console.error('Error reading the data file:', error);
       });
   }, []);
+  // For now, successfully returns the default search page. Will move elements to their own functions once operational
    return (
+      // A containing div so it's a single object return
       <div>
          <hr />
          <p>
+            {/* drop down menu for filyer type */}
             <label htmlFor="filters">Select data point to filter search by:</label>
             <select name="cars" id="filters">
                <option value="model">model</option>
@@ -28,16 +31,20 @@ export default function Search() {
             </select>
          </p>
          <p>
+            {/* text box for user entered keyword */}
             <label htmlFor="keyword">Keyword:</label>
             <input type="text" id="keyword" name="keyword" placeholder="Search by keyword"></input>
          </p>
-         <div id="searchStatus">No records to display</div>
+         {/* The search status gets its own div so its value can change as the app runs */}
+         <div id="searchStatus"><p>No Records to display</p></div>
+         {/* card placeholders for the various metric calculations required */}
          <div id="metricCards">
-            <div id="usageTime"></div>
-            <div id="screenTime"></div>
-            <div id="numberApps"></div>
-            <div id="age"></div>
+            <div id="usageTime">App Usage Time (min/day)</div>
+            <div id="screenTime">Screen On Time (hours/day)</div>
+            <div id="numberApps">Number of Apps Installed</div>
+            <div id="age">Age</div>
          </div>
+         {/* The actual table for the search results. Default is just the drop down menu for filyer type */}
          <div id="searchResultsTable">
             <table>
                <tr>
@@ -53,6 +60,8 @@ export default function Search() {
                   <th>Gender</th>
                   <th>User Behavior Class</th>
                </tr>
+               {/* Logic for searching will go here so it displays in the table correctly */}
+
             </table>
             <hr />
          </div>
