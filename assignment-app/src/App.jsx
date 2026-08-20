605176e4b7a7d852367a6513906d7211f1b9052b
Sean Simpson <JediTiger@users.noreply.github.com>
2026-08-19 19:49:42 -0500

    Landing page in progress

--- assignment-app/src/App.jsx
+++ assignment-app/src/App.jsx
@@ -1,11 +1,40 @@
 import React from "react";
-
+import { useState } from 'react';
+import { useEffect } from "react";
 function App() {
+
+// Starting with can i get it to import the data from the text file for the 'Home' page
+
+  function TextDisplayForHomePage() {
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    // Attempt to fetch the file from the public folder of this project
+    fetch('../public/dataset-information.txt')
+      .then((response) => response.text())
+      .then((data) => {
+        setText(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error('Error reading the text file:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h2>Document Content</h2>
+      <pre>{text}</pre>
+    </div>
+  );
+}
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         {/* <p> tag is a placeholder, You'll need to change the tag/component type later*/}
-        <p className="navbar-brand ms-4 nav-link">User Behavior Data</p>
+        <p className="navbar-brand ms-4 nav-link" id="home">User Behavior Data</p>
+        <p className="navbar-brand ms-4 nav-link" id="search">Search Behavior Data</p>
       </nav>
       <hr />
     </div>
@@ -13,3 +42,4 @@ function App() {
 }
 
 export default App;
+
