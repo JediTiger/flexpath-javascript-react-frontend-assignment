3073cccc9da4fb9a0968f1108e8a8129619ba5e3
Sean Simpson <JediTiger@users.noreply.github.com>
2026-08-24 00:03:07 -0500

    Starting on metric cards

--- assignment-app/src/computeFunctions.js
+++ assignment-app/src/computeFunctions.js
@@ -1,7 +1,10 @@
 
 export function computeAve(deciderVar, dataObject) {
-   if (dataObject === 0 || isNaN) return 0;
-
+   if (dataObject === 0 || isNaN) {
+      return 0;
+   } else {
+      
+   }
 
 
 }
@@ -14,3 +17,21 @@ export function computeMedian(deciderVar, dataObject) {
 
 }
 
+function decideComputeTarget(targetMetric) {
+   const decidedTarget;
+   switch(targetMetric) {
+      case "usage":
+         decidedTarget = 1;
+         break;
+      case "screen":
+         decidedTarget = 2;
+         break;
+      case "apps":
+         decidedTarget = 3;
+         break;
+      case "age":
+         decidedTarget = 4;
+         break;
+   }
+   return decidedTarget
+}
